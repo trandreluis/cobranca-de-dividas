f77b5a873f781f4b05e43f2222b4c23e537fbb5e
Andre Luis <tr.andreluis@gmail.com>
2017-01-18 18:00:55 -0300

    Implementacao do getConnection()

--- src/main/java/edu/ifpb/monteiro/ads/dao/BancoDerby.java
+++ src/main/java/edu/ifpb/monteiro/ads/dao/BancoDerby.java
@@ -1,8 +1,40 @@
 package edu.ifpb.monteiro.ads.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 
+ * @author Andre Luis
+ * 
+ * e-mail: tr.andreluis@gmail.com
+ *
+ */
+
 public class BancoDerby {
 
 	private String url = "jdbc:derby:banco-sas;create=true;user=derby;password=derby";
 	private String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+	
+	public Connection getConnection() {
+		
+		Connection conexao = null;
+		
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	
+		try {
+			conexao = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conexao;
+		
+	}
 	
 }
