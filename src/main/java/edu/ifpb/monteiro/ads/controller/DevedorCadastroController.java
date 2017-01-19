ce6cc874ce488b6d867a15339ac7e7b7ec1e6970
Andre Luis <tr.andreluis@gmail.com>
2017-01-19 18:43:22 -0300

    Criacao do Controller para a janela de cadastro de devedores

---
+++ src/main/java/edu/ifpb/monteiro/ads/controller/DevedorCadastroController.java
@@ -0,0 +1,15 @@
+package edu.ifpb.monteiro.ads.controller;
+
+/**
+ * 
+ * @author Andre Luis
+ * 
+ *         e-mail: tr.andreluis@gmail.com
+ *
+ */
+
+public class DevedorCadastroController {
+
+	
+	
+}
