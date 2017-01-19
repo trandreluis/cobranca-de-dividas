001d13075aa20bcc86dff4bca32c8a03686c2e64
Andre Luis <tr.andreluis@gmail.com>
2017-01-18 21:17:05 -0300

    Alteracao para um nome mais coeso

---
+++ src/main/java/edu/ifpb/monteiro/ads/model/Devedor.java
@@ -0,0 +1,70 @@
+package edu.ifpb.monteiro.ads.model;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author Andre Luis
+ * 
+ *         e-mail: tr.andreluis@gmail.com
+ *
+ */
+
+public class Devedor {
+
+	private long id;
+	private Divida divida;
+	private String nome;
+	private String cpf;
+	private Date dataNascimento;
+	private Endereco endereco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Divida getDivida() {
+		return divida;
+	}
+
+	public void setDivida(Divida divida) {
+		this.divida = divida;
+	}
+
+}
