056c247f36dc38cf89ea2885e28244a0516c4689
Andre Luis <tr.andreluis@gmail.com>
2017-01-20 00:26:56 -0300

    Implementacao do preenchimento da tabalea utilizando properties e observable list

--- src/main/java/edu/ifpb/monteiro/ads/controller/JanelaPrincipalController.java
+++ src/main/java/edu/ifpb/monteiro/ads/controller/JanelaPrincipalController.java
@@ -1,14 +1,22 @@
 package edu.ifpb.monteiro.ads.controller;
 
-import edu.ifpb.monteiro.ads.model.Devedor;
+
+import java.util.ArrayList;
+
+import edu.ifpb.monteiro.ads.dao.DevedorDao;
+import edu.ifpb.monteiro.ads.dao.DevedorPropertyDao;
+import edu.ifpb.monteiro.ads.model.DevedorAntigo;
+import edu.ifpb.monteiro.ads.model.DevedorProperty;
 import edu.ifpb.monteiro.ads.view.DevedorCadastroJanela;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * 
@@ -21,34 +29,53 @@ import javafx.scene.control.TableView;
 public class JanelaPrincipalController {
 
 	@FXML
-	AnchorPane root;
+	private AnchorPane root;
 	
 	@FXML
-	TableView<Devedor> tabelaDevedores;
+	private TableView<DevedorProperty> tabelaDevedores;
 	
 	@FXML
-	TableColumn<String, Devedor> colunaNomeDevedor;
+	private TableColumn<DevedorProperty, String> colunaNomeDevedor;
 	
 	@FXML
-	TableColumn<String, Devedor> colunaCidadeDevedor;
+	private TableColumn<DevedorProperty, String> colunaCidadeDevedor;
 	
 	@FXML
-	TableColumn<Integer, Devedor> colunaIdadeDevedor;
+	private TableColumn<DevedorProperty, Integer> colunaIdadeDevedor;
 	
 	@FXML
-	TableColumn<Double, Devedor> colunaDividaDevedor;
+	private TableColumn<DevedorProperty, Double> colunaDividaDevedor;
 
 	@FXML
-	Button botaoNovoDevedor;
+	private Button botaoNovoDevedor;
 	
 	@FXML
-	Button botaoEditarDevedor;
+	private Button botaoEditarDevedor;
 	
 	@FXML
-	Button botaoExcluirDevedor;
+	private Button botaoExcluirDevedor;
 	
 	@FXML
-	Button botaoNegociar;
+	private Button botaoNegociar;
+	
+	@FXML
+	public void initialize() {
+		
+		ObservableList<DevedorProperty> devedoresObservableList = FXCollections.observableArrayList();
+		
+		DevedorPropertyDao dao = new DevedorPropertyDao();
+		
+		ArrayList<DevedorProperty> devedoresList = dao.buscarTodos();
+		
+		for(DevedorProperty d : devedoresList) {
+			devedoresObservableList.add(d);
+		}
+		
+		colunaNomeDevedor.setCellValueFactory(valorDaCelula -> valorDaCelula.getValue().nome());
+		
+		tabelaDevedores.setItems(devedoresObservableList);
+		
+	}
 	
 	
 	public void novoDevedor() {
@@ -105,5 +132,5 @@ public class JanelaPrincipalController {
 		alerta.showAndWait();
 
 	}
-	
+
 }
