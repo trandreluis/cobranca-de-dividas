d92b048cd52fe3040e951052c2b8a5d365887e8b
Andre Luis <tr.andreluis@gmail.com>
2017-01-19 22:42:40 -0300

    Implementacao do carregamento do FXML

---
+++ src/main/java/edu/ifpb/monteiro/ads/view/DevedorCadastroJanela.java
@@ -0,0 +1,44 @@
+package edu.ifpb.monteiro.ads.view;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class DevedorCadastroJanela extends Stage {
+
+	AnchorPane root;
+
+	public DevedorCadastroJanela(Pane pai) {
+
+		setResizable(false);
+		setTitle("Cadastro de Devedor");
+
+		// Define a janela principal como dona desta
+		initOwner(pai.getScene().getWindow());
+
+		// Define o icone
+		getIcons().add(new Image("icone/cedula.png"));
+
+		// Define esta janela como modal, nao permitindo que se mexa na
+		// principal enquanto esta nao for finalizada
+		initModality(Modality.WINDOW_MODAL);
+		
+		try {
+			root = FXMLLoader.load(getClass().getResource("/fxml/DevedorCadastro.fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Scene cena = new Scene(root);
+		setScene(cena);
+
+	}
+
+}
