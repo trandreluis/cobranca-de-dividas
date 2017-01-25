9c352b6f239cb5e8b001df3967cc2b2b287894a4
Andre Luis <tr.andreluis@gmail.com>
2017-01-24 23:38:31 -0300

    Modificacao do Dao de acesso aos dados

--- src/main/java/edu/ifpb/monteiro/ads/controller/JanelaPrincipalController.java
+++ src/main/java/edu/ifpb/monteiro/ads/controller/JanelaPrincipalController.java
@@ -1,12 +1,9 @@
 package edu.ifpb.monteiro.ads.controller;
 
-
 import java.util.ArrayList;
 
 import edu.ifpb.monteiro.ads.dao.DevedorDao;
-import edu.ifpb.monteiro.ads.dao.DevedorPropertyDao;
-import edu.ifpb.monteiro.ads.model.DevedorAntigo;
-import edu.ifpb.monteiro.ads.model.DevedorProperty;
+import edu.ifpb.monteiro.ads.model.Devedor;
 import edu.ifpb.monteiro.ads.view.DevedorCadastroJanela;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -30,78 +27,75 @@ public class JanelaPrincipalController {
 
 	@FXML
 	private AnchorPane root;
-	
+
 	@FXML
-	private TableView<DevedorProperty> tabelaDevedores;
-	
+	private TableView<Devedor> tabelaDevedores;
+
 	@FXML
-	private TableColumn<DevedorProperty, String> colunaNomeDevedor;
-	
+	private TableColumn<Devedor, String> colunaNomeDevedor;
+
 	@FXML
-	private TableColumn<DevedorProperty, String> colunaCidadeDevedor;
-	
+	private TableColumn<Devedor, String> colunaCidadeDevedor;
+
 	@FXML
-	private TableColumn<DevedorProperty, Integer> colunaIdadeDevedor;
-	
+	private TableColumn<Devedor, Integer> colunaIdadeDevedor;
+
 	@FXML
-	private TableColumn<DevedorProperty, Double> colunaDividaDevedor;
+	private TableColumn<Devedor, Double> colunaDividaDevedor;
 
 	@FXML
 	private Button botaoNovoDevedor;
-	
+
 	@FXML
 	private Button botaoEditarDevedor;
-	
+
 	@FXML
 	private Button botaoExcluirDevedor;
-	
+
 	@FXML
 	private Button botaoNegociar;
-	
+
 	@FXML
 	public void initialize() {
-		
-		ObservableList<DevedorProperty> devedoresObservableList = FXCollections.observableArrayList();
-		
-		DevedorPropertyDao dao = new DevedorPropertyDao();
-		
-		ArrayList<DevedorProperty> devedoresList = dao.buscarTodos();
-		
-		for(DevedorProperty d : devedoresList) {
+
+		ObservableList<Devedor> devedoresObservableList = FXCollections.observableArrayList();
+
+		DevedorDao dao = new DevedorDao();
+
+		ArrayList<Devedor> devedoresList = dao.buscarTodos();
+
+		for (Devedor d : devedoresList) {
 			devedoresObservableList.add(d);
 		}
-		
+
 		colunaNomeDevedor.setCellValueFactory(valorDaCelula -> valorDaCelula.getValue().nome());
-		
+
 		tabelaDevedores.setItems(devedoresObservableList);
-		
+
 	}
-	
-	
+
 	public void novoDevedor() {
 
 		Alert alerta = new Alert(AlertType.INFORMATION);
 
 		alerta.setTitle("Informacao");
 		alerta.setHeaderText("Sucesso!");
-		alerta.setContentText(
-				"Cadastrar novo devedor!");
+		alerta.setContentText("Cadastrar novo devedor!");
 
 		alerta.showAndWait();
-		
+
 		DevedorCadastroJanela devedorCadastro = new DevedorCadastroJanela(root);
 		devedorCadastro.show();
 
 	}
-	
+
 	public void editarDevedor() {
 
 		Alert alerta = new Alert(AlertType.INFORMATION);
 
 		alerta.setTitle("Informacao");
 		alerta.setHeaderText("Sucesso!");
-		alerta.setContentText(
-				"Editar devedor!");
+		alerta.setContentText("Editar devedor!");
 
 		alerta.showAndWait();
 
@@ -113,21 +107,19 @@ public class JanelaPrincipalController {
 
 		alerta.setTitle("Informacao");
 		alerta.setHeaderText("Sucesso!");
-		alerta.setContentText(
-				"Excluir devedor!");
+		alerta.setContentText("Excluir devedor!");
 
 		alerta.showAndWait();
 
 	}
-	
+
 	public void negociarComDevedor() {
 
 		Alert alerta = new Alert(AlertType.INFORMATION);
 
 		alerta.setTitle("Informacao");
 		alerta.setHeaderText("Sucesso!");
-		alerta.setContentText(
-				"Negociar com devedor!");
+		alerta.setContentText("Negociar com devedor!");
 
 		alerta.showAndWait();
 
