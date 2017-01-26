98bf154f86d54d751b996c69140fa637409a8719
Andre Luis <tr.andreluis@gmail.com>
2017-01-26 01:26:58 -0300

    Implementacao do getController()

--- src/main/java/edu/ifpb/monteiro/ads/view/MainApp.java
+++ src/main/java/edu/ifpb/monteiro/ads/view/MainApp.java
@@ -1,5 +1,6 @@
 package edu.ifpb.monteiro.ads.view;
 
+import edu.ifpb.monteiro.ads.controller.JanelaPrincipalController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -19,6 +20,7 @@ public class MainApp extends Application {
 
 	private AnchorPane root;
 	private static Stage primaryStage;
+	private static FXMLLoader fxml;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -28,19 +30,28 @@ public class MainApp extends Application {
 		//Definindo icone
 		MainApp.primaryStage.getIcons().add(new Image("/icone/cedula.png"));
 		
+		fxml = new FXMLLoader(getClass().getResource("/fxml/JanelaPrincipal.fxml"));
+		
 		//Definindo e carregando o FXML pra a janela principal
-		root = FXMLLoader.load(getClass().getResource("/fxml/JanelaPrincipal.fxml"));
+		root = (AnchorPane) fxml.load();
 		
 		//Definicao da Scena a ser exibida
 		Scene cena = new Scene(root);
 		MainApp.primaryStage.setScene(cena);
 		MainApp.primaryStage.show();
 		
-		//Encerrar a aplicacao quando a janela princiapl for fechada
+		//Encerrar a aplicacao quando a janela principal for fechada
 		MainApp.primaryStage.setOnCloseRequest(e -> System.exit(0));
 		
 	}
 
+	public static JanelaPrincipalController getController() {
+
+		JanelaPrincipalController controller = (JanelaPrincipalController) fxml.getController();
+		
+		return controller;
+	}
+	
 	public static Stage getPrimaryStage() {
         return primaryStage;
     }
