9a97ccaee8e86da593bff35715d2e5e762b2fce0
Andre Luis <tr.andreluis@gmail.com>
2017-01-28 08:24:32 -0300

    Chamada para criar o banco caso não haja e definicao de titulo da janela

--- src/main/java/edu/ifpb/monteiro/ads/view/MainApp.java
+++ src/main/java/edu/ifpb/monteiro/ads/view/MainApp.java
@@ -1,5 +1,10 @@
 package edu.ifpb.monteiro.ads.view;
 
+import java.io.File;
+
+import javax.swing.JOptionPane;
+
+import edu.ifpb.monteiro.ads.banco.Tabelas;
 import edu.ifpb.monteiro.ads.controller.JanelaPrincipalController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -21,43 +26,58 @@ public class MainApp extends Application {
 	private AnchorPane root;
 	private static Stage primaryStage;
 	private static FXMLLoader fxml;
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		MainApp.primaryStage = primaryStage;
 		
-		//Definindo icone
+		primaryStage.setTitle("SACD - Sistema de Apoio a Cobrança de Dividas");
+
+		// Definindo icone
 		MainApp.primaryStage.getIcons().add(new Image("/icone/cedula.png"));
-		
+
 		fxml = new FXMLLoader(getClass().getResource("/fxml/JanelaPrincipal.fxml"));
-		
-		//Definindo e carregando o FXML pra a janela principal
+
+		// Definindo e carregando o FXML pra a janela principal
 		root = (AnchorPane) fxml.load();
-		
-		//Definicao da Scena a ser exibida
+
+		// Definicao da Scena a ser exibida
 		Scene cena = new Scene(root);
 		MainApp.primaryStage.setScene(cena);
 		MainApp.primaryStage.show();
-		
-		//Encerrar a aplicacao quando a janela principal for fechada
+
+		// Encerrar a aplicacao quando a janela principal for fechada
 		MainApp.primaryStage.setOnCloseRequest(e -> System.exit(0));
-		
+
 	}
 
 	public static JanelaPrincipalController getController() {
 
 		JanelaPrincipalController controller = (JanelaPrincipalController) fxml.getController();
-		
+
 		return controller;
 	}
-	
+
 	public static Stage getPrimaryStage() {
-        return primaryStage;
-    }
-	
+		return primaryStage;
+	}
+
 	public static void main(String[] args) {
+
+		File file = new File("banco-dividas/");
+
+		if (file.exists()) {
+			JOptionPane.showMessageDialog(null, "Banco de dados local encontrado!");
+		}
+
+		else {
+			JOptionPane.showMessageDialog(null, "Criando banco de dados local!");
+			Tabelas tabelas = new Tabelas();
+			tabelas.criarBanco();
+		}
+
 		launch(args);
 	}
-	
+
 }
